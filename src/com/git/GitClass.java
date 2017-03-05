2411f2d5a2d24efaa76b75f508415c138f57febe
sanae <sanae.hadachi@gmail.com>
2017-03-05 22:51:33 +0100

    La création de la premiere classe

---
+++ src/com/git/GitClass.java
@@ -0,0 +1,14 @@
+package com.git;
+
+public class GitClass {
+
+	public GitClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
